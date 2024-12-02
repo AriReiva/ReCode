36a6e3345387d40795c2920914b2fb6208d4e44d
AriReiva <157453010+AriReiva@users.noreply.github.com>
2024-12-02 16:54:58 +0700

    Update Siswa.kt

--- app/src/main/java/com/example/recode/Siswa.kt
+++ app/src/main/java/com/example/recode/Siswa.kt
@@ -1,12 +1,12 @@
 package com.example.recode
 
 data class Siswa(
-    val id: Int = 0,        // ID siswa (opsional, SQLite bisa autoincrement)
-    val nama: String,       // Nama siswa
-    val nisn: String,       // NISN siswa
-    val kelas: String,      // Kelas siswa
-    val jurusan: String,    // Jurusan siswa
-    val gender: String,     // Gender siswa
-    val password: String    // Password siswa
+    val id: Int = 0, 
+    val nama: String,    
+    val nisn: String, 
+    val kelas: String,   
+    val jurusan: String,   
+    val gender: String,  
+    val password: String   
 )
 
