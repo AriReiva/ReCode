db08efe7d913101c1d345007e73ce46914b03c11
AriReiva <157453010+AriReiva@users.noreply.github.com>
2024-12-02 16:54:26 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/recode/MainActivity.kt
+++ app/src/main/java/com/example/recode/MainActivity.kt
@@ -11,23 +11,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Cek database
         val dbHelper = DBHelper(this)
         val hasUserData = dbHelper.hasUserData()
 
-        // Timer untuk splash screen
         Handler().postDelayed({
             if (hasUserData) {
-                // Jika ada data, arahkan ke LoginActivity
                 val intent = Intent(this, LoginActivity::class.java)
                 startActivity(intent)
             } else {
-                // Jika tidak ada data, arahkan ke SignUpActivity
                 val intent = Intent(this, SignUpActivity::class.java)
                 startActivity(intent)
             }
             finish()
-        }, 2000) // 2 detik splash screen
+        }, 2000)
 
     }
 }
